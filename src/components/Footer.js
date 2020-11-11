996d90d1cc61850f9ed636637bf15cdf22bfe489
Beasley <beasley.com@gmail.com>
2020-11-11 00:09:07 -0800

    got basic setup

---
+++ src/components/Footer.js
@@ -0,0 +1,4 @@
+export default function Footer(){
+    return (
+        <p className="footer">&copy; @beasleydotcom 2020</p>    )
+}
---
+++ src/components/Main.js
@@ -0,0 +1,10 @@
+export default function Main(){
+    return (
+        <>
+            <h2>body of the message</h2>
+            <p>I am so glad I've found an industry which prizes teamwork, creative problem solving, and retrospective/iterative planning because I think these are some of my biggest assets.
+            Driven by a desire to be helpful, I am an enthusiastic student of Software Development because I believe it is the realm in which I can do the most good for humanity and have the most fun.
+            </p>
+        </>
+    )
+}
